670346fdf0660b7474595cedaae0fcacec4ec551
MarcoSettembre <marco.settembre001@studenti.uniparthenope.it>
2026-04-30 13:31:31 +0200

    added new models to implement user-specific data and permissions

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Banchina(models.Model):
     numero = models.IntegerField(db_column='Numero')
@@ -173,3 +173,30 @@ class TappeItinerario(models.Model):
         managed = True
         db_table = 'Tappe_itinerario'
         unique_together = (('id_itinerario', 'tappa'),)
+class UserCliente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.user.username} -> Cliente {self.cliente.codice_fiscale} {self.cliente.nome} {self.cliente.cognome}"
+class UserNave(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    nave = models.ForeignKey(Nave, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = (('user', 'nave'),)
+    def __str__(self):
+        return f"{self.user.username} -> Nave {self.nave.imo} {self.nave.nome}"
+class UserMagazzino(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    magazzino = models.ForeignKey(Magazzino, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = (('user', 'magazzino'),)
+    def __str__(self):
+        return f"{self.user.username} -> Magazzino {self.magazzino.nome} {self.magazzino.localita}"
+class UserBanchina(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    numero_banchina = models.IntegerField(default=0)
+    settore_banchina = models.IntegerField(default=0)
+    class Meta:
+        unique_together = (('user', 'numero_banchina', 'settore_banchina'),)
+    def __str__(self):
+        return f"{self.user.username} -> Banchina {self.numero_banchina} {self.settore_banchina}"
